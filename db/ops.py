d946a23969993833db9974d34b835fb4970ca147
Jonathan Mishler <jonathan.mishler@gmail.com>
2022-01-17 20:41:39 -0600

    refactor insert records

--- db/ops.py
+++ db/ops.py
@@ -1,7 +1,7 @@
 from typing import Type, Iterable, Optional, List
 import logging
 from pydantic import fields
-from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.dialects import postgresql
 from sqlalchemy.inspection import inspect
 import psycopg
 from psycopg import sql
@@ -12,8 +12,8 @@ from .setup import engine
 def gen_data_model(
     model: Type[SQLModel],
     data: Iterable,
-    exclude: Optional[dict] = None,
     as_dict: bool = False,
+    exclude: Optional[dict] = None
 ):
     """Generator returning each row of the parsed data model with the option
     to return as a dict or tuple with excludable fields.  export_type can be
@@ -26,11 +26,16 @@ def gen_data_model(
         yield row
 
 
-def pg_insert_stmt(model: Type[SQLModel], records: List[dict]):
+def insert_stmt(model: Type[SQLModel], skip_conflit: bool = False):
     """Using sqlalchemy's postgres dialect support, create the insert statement
     with conflict resolution
     """
-    return insert(model).values(records).on_conflict_do_nothing()
+    if skip_conflit:
+        stmt = postgresql.insert(model).on_conflict_do_nothing()
+    else:
+        stmt = model.__table__.insert()
+    
+    return stmt
 
 
 def exclude_auto_pk_fields(model):
@@ -45,33 +50,23 @@ def insert_records(model: Type[SQLModel], data: Iterable, skip_conflict: bool = 
     """Inserts the data into the database and if it is a postgres database we can use the
     conflict resolution to skip records which already exsist in our table"""
 
-    if skip_conflict:
-        logging.info("Using Postgres Dialect to skip on conflict")
-        exclude_fields = exclude_auto_pk_fields(model)
-        as_dict = True
-    elif not skip_conflict:
-        logging.info("Using standard sqlalchemy method")
-        exclude_fields = None
-        as_dict = False
+    exclude_fields = exclude_auto_pk_fields(model)
     records = list()
     i = 0
+    stmt = insert_stmt(model, skip_conflict)
     with Session(engine) as session:
-        for record in gen_data_model(model, data, exclude_fields, as_dict):
+        for record in gen_data_model(model, data, True, exclude_fields):
             if record:
-                if skip_conflict:
-                    records.append(record)
-                elif not skip_conflict:
-                    session.add(record)
+                records.append(record)
                 i += 1
-                if i == 10000:
-                    if skip_conflict:
-                        session.execute(pg_insert_stmt(model, records))
-                        records.clear()
-                    session.commit()
-                    i = 0
+
+            if i == 10000:
+                session.execute(stmt, records)
+                session.commit()
+                records.clear()
+                i = 0
         if i > 0:
-            if skip_conflict:
-                session.execute(pg_insert_stmt(model, records))
+            session.execute(stmt, records)
             session.commit()
 
 
